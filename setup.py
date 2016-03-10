c4c64f35dfad657a1103cdd2d17723ce54c4265f
christopher <christoipher.chen@gmail.com>
2016-03-09 21:36:28 -0800

    published to PIP

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pyrpn',
+setup(name='pyrpncalc',
       version='0.1',
-      description='A Reverse Polish Notation (postfix) module',
+      description='A Reverse Polish Notation (postfix) module with extensive operation support.',
       url='https://bitbucket.org/TheCDC/pyrpn',
       author='Christopher Chen',
       author_email='christopher.chen1995@gmail.com',
       license='Public Domain',
-      packages=['pyrpn'],
-      zip_safe=False)
+      packages=['pyrpncalc'],
+      zip_safe=False)	
