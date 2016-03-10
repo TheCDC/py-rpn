53416f7e6e71a7abf10920c4d4eabb25f53d3f41
Christopher Chen <christoipher.chen@gmail.com>
2016-03-10 02:40:21 -0800

    Attempt to allow pip3

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(name='pyrpncalc',
-      version='0.1.10',
+      version='0.1.11',
       description="""A Reverse Polish Notation (postfix) module with extensive operation support.""",
       long_description="Use your favorite numeric input style, RPN, right on your desktop! This module is primarily designed for use in interactive mode, but may also be used by other modules. Run with 'python3 -m pyrpncalc'",
+      classifiers=[
+'Programming Language :: Python :: 3.5']
       keywords="math calculator utility command line",
       url='https://bitbucket.org/TheCDC/pyrpn',
       author='Christopher Chen',
