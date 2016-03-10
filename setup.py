da9899b63f578c764e95e7def999bbe6142dc1a8
christopher <christoipher.chen@gmail.com>
2016-03-09 20:46:16 -0800

    Began refactoring for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyrpn',
+      version='0.1',
+      description='A Reverse Polish Notation (postfix) module',
+      url='https://bitbucket.org/TheCDC/pyrpn',
+      author='Christopher Chen',
+      author_email='christopher.chen1995@gmail.com',
+      license='Public Domain',
+      packages=['pyrpn'],
+      zip_safe=False)
