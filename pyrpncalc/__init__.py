9b5c14e9a1112ffd60a5c004a8a4dce5c0d3437e
christopher <christoipher.chen@gmail.com>
2016-03-10 00:32:50 -0800

    Improved help

--- pyrpncalc/__init__.py
+++ pyrpncalc/__init__.py
@@ -265,17 +265,26 @@ class stackRPN():
         Prints out commands and other help.
         """
         args = 0
-        helpstr = """Welcome to the RPN calculator in Python!
+        helpstr = """Welcome to the Reverse Polish Notation calculator in Python!
         Enter math functions and arithmetic operators in RPN.
         Separate names (sin, pi, e, etc.) and numbers by spaces, don't worry about spaces for operators (+,-,*,/).
         Examples:
-            "1 1+" => 2
-            "1 2 + 3 4 + /" => 0.428571428571
-            "pi sin" => 0.000000000000
-            "2 sqrt 2 sqrt *" => 2.000000000000
-            "2 2 inv ^ 2 ^" => 2.000000000000
-            "10 cos 2 ^ 10 sin 2 ^ +" => 1.000000000000
-            "2 dup +" => 4.000000000000
+            "1 1+" = (1+1)
+                => 2
+            "1 2 + 3 4 + /" = (1+2)/(3+4)
+                => 0.428571428571
+            "pi sin" = sin(pi)
+                => 0.000000000000
+            "2 sqrt 2 sqrt *" = sqrt(2)*sqrt(2)
+                => 2.000000000000
+            "2 2 inv ^ 2 ^" = (2^(1/2))^2
+                => 2.000000000000
+            "10 cos 2 ^ 10 sin 2 ^ +" = cos(10)^2 + sin(10)^2
+                => 1.000000000000
+            "2 dup +" = (2+2)
+                => 4.000000000000
+            "e pi n ^" = e^(-pi)
+                => 0.043213918264
         Here are some commands:"""
         print(helpstr)
         for i in sorted(self.cmddict.keys()):
