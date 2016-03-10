023b819fd17959b62738ea93bc38f2806fcd1570
christopher <christoipher.chen@gmail.com>
2016-03-09 21:58:45 -0800

    edited description

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(name='pyrpncalc',
       version='0.1',
-      description='A Reverse Polish Notation (postfix) module with extensive operation support.',
+      description="""A Reverse Polish Notation (postfix) module with extensive operation support.
+      It is primarily designed for use in interactive mode, but may also be used by other modules.""",
       url='https://bitbucket.org/TheCDC/pyrpn',
       author='Christopher Chen',
       author_email='christopher.chen1995@gmail.com',
